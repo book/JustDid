b4f15347a09e8f50cddb82010af584c5409a9a6e
Philippe Bruhat (BooK) <book@cpan.org>
2020-06-09 14:55:28 +0200

    First working prototype!
    We added:
    - a new activity, and pointed to it as the default activity
    - a layout with input, submit button and result log area
    - the actual code to react to the button being pressed

--- app/src/main/java/net/bruhat/justdid/MainActivity.kt
+++ app/src/main/java/net/bruhat/justdid/MainActivity.kt
@@ -2,10 +2,31 @@ package net.bruhat.justdid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun millisToString(millis: Long): String {
+        val pattern = "yyyy-MM-dd HH:mm:ss Z"
+        val simpleDateFormat = SimpleDateFormat(pattern)
+        val dateTimeStr = simpleDateFormat.format(Date(millis))
+        return dateTimeStr
+    }
+
+    fun addToTaskLog(view: View) {
+        val taskLog = findViewById<TextView>(R.id.taskLog)
+        val task    = findViewById<EditText>(R.id.editText)
+        val dateTimeStr = millisToString(System.currentTimeMillis())
+        val newText = dateTimeStr + " " + task.text + "\n" + taskLog.text
+        taskLog.setText( newText.toCharArray(), 0, newText.length )
+        task.setText("")
     }
 }
