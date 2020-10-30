59b51ee4825f36e9efb1a11b8bf0166cf307a9f1
Eric Herman <eric@freesa.org>
2020-10-30 09:49:22 +0000

    Add TimeZone to Clock interface

--- app/src/main/java/net/bruhat/justdid/Clock.java
+++ app/src/main/java/net/bruhat/justdid/Clock.java
@@ -1,19 +1,26 @@
 package net.bruhat.justdid;
 
+import java.util.TimeZone;
+
 public interface Clock {
     public long currentTimeMillis();
+    public TimeZone timeZone();
 
     public class System implements Clock {
         public long currentTimeMillis() {
             return java.lang.System.currentTimeMillis();
         }
+        public TimeZone timeZone() { return TimeZone.getDefault(); }
     }
 
     public class Fake implements Clock {
         public long millis = 0;
+        public TimeZone timeZone = null;
 
         public long currentTimeMillis() {
             return millis;
         }
+
+         public TimeZone timeZone() { return timeZone; }
     }
 }
