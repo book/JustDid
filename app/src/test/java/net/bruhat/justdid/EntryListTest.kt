03e9977341a6624590f49fe5455467f10ac23bee
Philippe Bruhat (BooK) <book@cpan.org>
2020-10-30 08:47:24 +0000

    Test persistence
    Co-authored-by: Eric Herman <eric@freesa.org>

--- app/src/test/java/net/bruhat/justdid/EntryListTest.kt
+++ app/src/test/java/net/bruhat/justdid/EntryListTest.kt
@@ -1,11 +1,8 @@
 package net.bruhat.justdid
 
-import android.content.Context
-import java.util.TimeZone
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import java.io.File
 
 class EntryListTest {
@@ -24,20 +21,35 @@ class EntryListTest {
     {
     }
 
+    fun write_four_entries(persistedLog: File,  testClock: Clock.Fake )
+    {
+        var chorelist = EntryList(persistedLog, testClock)
+        chorelist.addEntry( "chore 1")
+        testClock.millis++;
+        chorelist.addEntry( "chore 2")
+        chorelist.addEntry( "chore 3")
+        testClock.millis++;
+        chorelist.addEntry( "chore 4")
+        assertEquals( chorelist.entries[0].label, "chore 4")
+        assertEquals( chorelist.entries[1].label, "chore 3")
+        assertEquals( chorelist.entries[2].label, "chore 2")
+        assertEquals( chorelist.entries[3].label, "chore 1")
+        chorelist.save()
+    }
+
     @Test
     fun entries_added_at_front() {
         var testClock = Clock.Fake();
         testClock.millis = 1585702923;
 
         var persistedLog = File.createTempFile("EntryListTest_", ".tmp")
+        write_four_entries(persistedLog, testClock)
 
         var chorelist = EntryList(persistedLog, testClock)
-        chorelist.addEntry( "chore 1")
-        chorelist.addEntry( "chore 2")
-        chorelist.addEntry( "chore 3")
-        assertEquals( chorelist.entries[0].label, "chore 3")
-        assertEquals( chorelist.entries[1].label, "chore 2")
-        assertEquals( chorelist.entries[2].label, "chore 1")
+        assertEquals( chorelist.entries[0].label, "chore 4")
+        assertEquals( chorelist.entries[1].label, "chore 3")
+        assertEquals( chorelist.entries[2].label, "chore 2")
+        assertEquals( chorelist.entries[3].label, "chore 1")
     }
 
 }
