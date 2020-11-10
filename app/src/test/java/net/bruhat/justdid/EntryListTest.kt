0ca7d041c11ea28a22f105dc61d8798428908032
Eric Herman <eric@freesa.org>
2020-11-10 08:02:37 +0000

    Reformat code
    Co-authored-by: Philippe Bruhat (BooK) <book@cpan.org>

--- app/src/test/java/net/bruhat/justdid/EntryListTest.kt
+++ app/src/test/java/net/bruhat/justdid/EntryListTest.kt
@@ -7,19 +7,18 @@ import java.io.File
 
 class EntryListTest {
 
-    fun write_four_entries(persistedLog: File,  testClock: Clock.Fake )
-    {
+    fun write_four_entries(persistedLog: File, testClock: Clock.Fake) {
         var chorelist = EntryList(persistedLog, testClock)
-        chorelist.addEntry( "chore 1")
+        chorelist.addEntry("chore 1")
         testClock.millis++;
-        chorelist.addEntry( "chore 2")
-        chorelist.addEntry( "chore 3")
+        chorelist.addEntry("chore 2")
+        chorelist.addEntry("chore 3")
         testClock.millis++;
-        chorelist.addEntry( "chore 4")
-        assertEquals( chorelist.entries[0].label, "chore 4")
-        assertEquals( chorelist.entries[1].label, "chore 3")
-        assertEquals( chorelist.entries[2].label, "chore 2")
-        assertEquals( chorelist.entries[3].label, "chore 1")
+        chorelist.addEntry("chore 4")
+        assertEquals(chorelist.entries[0].label, "chore 4")
+        assertEquals(chorelist.entries[1].label, "chore 3")
+        assertEquals(chorelist.entries[2].label, "chore 2")
+        assertEquals(chorelist.entries[3].label, "chore 1")
         chorelist.save()
     }
 
@@ -39,10 +38,10 @@ class EntryListTest {
         write_four_entries(persistedLog, testClock)
 
         var chorelist = EntryList(persistedLog, testClock)
-        assertEquals( chorelist.entries[0].label, "chore 4")
-        assertEquals( chorelist.entries[1].label, "chore 3")
-        assertEquals( chorelist.entries[2].label, "chore 2")
-        assertEquals( chorelist.entries[3].label, "chore 1")
+        assertEquals(chorelist.entries[0].label, "chore 4")
+        assertEquals(chorelist.entries[1].label, "chore 3")
+        assertEquals(chorelist.entries[2].label, "chore 2")
+        assertEquals(chorelist.entries[3].label, "chore 1")
     }
 
     @Test
@@ -60,6 +59,6 @@ class EntryListTest {
 
         labels.clear()
         chorelist.forTopN(15, { labels.add(it.label) });
-        assertEquals( 4, labels.size)
+        assertEquals(4, labels.size)
     }
 }
